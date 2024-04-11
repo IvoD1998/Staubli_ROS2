798bb5b8936f5bc4b599fa151c21bb2ded92a038
Ivo Dekker <ivo.dekker@kuleuven.be>
2024-04-11 14:15:22 +0200

    automatic assignment of robot IP for middleware components via launch

--- adaptive_motion_control/robot_middleware/launch/robot_middleware.launch.py
+++ adaptive_motion_control/robot_middleware/launch/robot_middleware.launch.py
@@ -1,12 +1,25 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 from pathlib import Path
 
 parameters_file_path = Path(get_package_share_directory('robot_middleware'), 'config', 'settings.yaml')
 
 def generate_launch_description():
-
+     #Define launch arguments
+    launch_arguments=[]
+    #Declare arguments
+    launch_arguments.append(
+        DeclareLaunchArgument(
+            "robot_ip",
+            description="IP address of the real robot"
+        )
+    )
+    robot_ip = LaunchConfiguration(
+        "robot_ip"
+    )
     #Nodes
     nodes = []
     nodes.append(
@@ -15,7 +28,8 @@ def generate_launch_description():
             executable="robot_middleware",
             output="screen",
             parameters=[
-                parameters_file_path
+                parameters_file_path,
+                {"robot_ip": robot_ip}
             ],
         )
     )
